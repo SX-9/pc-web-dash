8646dbaa11d3e48c602cd61d9c575b39ec7952cd
SX Spy Agent <satriakrach@gmail.com>
2022-08-25 23:26:23 +0000

    Added a main page (more later)

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const app = require('express')();
 const osu = require('node-os-utils');
+const fs = require('fs');
 const cors = require('cors')({ orgin: '*' });
 const port = process.argv[3] || 80;
 
 app.use(cors);
-app.get('/', (req, res) => res.json({
+app.get('/', (req, res) => res.send(fs.readFileSync('index.html', { encoding:'utf8' })));
+app.get('/api', (req, res) => res.json({
     endpoints: [ '/cpu', '/ram', '/osi', ],
 }));
-app.get('/cpu', (req, res) => osu.cpu.usage().then(v => res.json(v)));
-app.get('/ram', (req, res) => osu.mem.used().then(v => res.json(v)));
-app.get('/osi', (req, res) => res.json({ 
+app.get('/api/cpu', (req, res) => osu.cpu.usage().then(v => res.json(v)));
+app.get('/api/ram', (req, res) => osu.mem.used().then(v => res.json(v)));
+app.get('/api/osi', (req, res) => res.json({ 
     platform: osu.os.platform(),
     uptime: osu.os.uptime(),
     hostname: osu.os.hostname(),
